e7a605e7ca35d64a89511e4fa305849304bf0bc5
cwong2015 <sirhc8002@gmail.com>
2017-06-30 11:58:01 -0400

    added bootstrap modal fade in pop-up when user clicks search

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   // $(".title").css("display", "inline");
   $(".title").fadeIn(1500);
-  $("#button1").click(function(){
-   var query = $("#input").val();
-   $("#test").html(query);
-  });
+//  $("#button1").click(function(){
+//   var query = $("#input").val();
+//   $("#test").html(query);
+//  });
   $("#button2").click(function(){
     window.open("https://en.wikipedia.org/wiki/Special:Random");
   });
